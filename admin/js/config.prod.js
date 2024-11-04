345aab3533d8502c8889cd4aaf972215d4d9be99
Dorian Maquet <takenzmr@gmail.com>
2024-11-04 11:05:59 +0100

    Premier changement

---
+++ admin/js/config.prod.js
@@ -0,0 +1,9 @@
+// Ce fichier contiendra uniquement les clés de l'environnement de production
+export const firebaseConfig = {
+    apiKey: "votre-cle-api-production",
+    authDomain: "votre-app-prod.firebaseapp.com",
+    projectId: "votre-app-prod",
+    storageBucket: "votre-app-prod.appspot.com",
+    messagingSenderId: "xxxxx",
+    appId: "1:xxxxx:web:xxxxx"
+};
