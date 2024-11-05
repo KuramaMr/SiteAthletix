cb6e6e6893df924f7e9f7c9acecfed5b47a2467b
Dorian Maquet <takenzmr@gmail.com>
2024-11-05 13:24:59 +0100

    Deploy Netlify

--- admin/js/config.js
+++ admin/js/config.js
@@ -1,20 +1,22 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY || "VOTRE_CLE_API",
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN || "VOTRE_DOMAINE",
-    projectId: process.env.FIREBASE_PROJECT_ID || "VOTRE_PROJET",
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "VOTRE_BUCKET",
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "VOTRE_SENDER",
-    appId: process.env.FIREBASE_APP_ID || "VOTRE_APP_ID"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 export const config = {
-    imgbbApiKey: process.env.IMGBB_API_KEY || "VOTRE_CLE_IMGBB"
+    imgbbApiKey: process.env.IMGBB_API_KEY
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
