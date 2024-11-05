ff3cdd3f93b03ad7566bb334056693bc871b4c7a
Dorian Maquet <takenzmr@gmail.com>
2024-11-05 12:51:48 +0100

    Deploy Netlify

--- admin/js/login.js
+++ admin/js/login.js
@@ -1,4 +1,4 @@
-import { auth } from '/SiteAthletix/admin/js/config.js';
+import { auth } from './config.js';
 import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
 
 const loginForm = document.getElementById('loginForm');
